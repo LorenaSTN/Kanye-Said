f6a750d0a509b86a73f39ea9c9d264e5c3e68be8
LorenaSTN <lorenstn@hotmail.com>
2024-11-25 18:27:56 +0100

    first modifying options

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,33 @@
-'use strict';
+"use strict";
 
-console.log('>> Ready :)');
+const cardTitle = document.querySelector(".title-js");
+const inputName = document.querySelector(".inputName-js");
+const cardName = document.querySelector(".name-js");
+
+const setMessage = () => {
+  const selectedMessage = document.querySelector(
+    'input[name="message"]:checked'
+  );
+
+  if (selectedMessage.value === "birthday") {
+    cardTitle.innerHTML = "Happy Birthday";
+  } else if (selectedMessage.value === "sorry") {
+    cardTitle.innerHTML = "Sorry";
+  } else {
+    cardTitle.innerHTML = "Love you";
+  }
+};
+
+document.querySelectorAll("input[name=message]").forEach((radio) => {
+  radio.addEventListener("change", setMessage);
+});
+
+setMessage();
+
+const setReceiver = (ev) => {
+  ev.preventDefault();
+  const value = inputName.value;
+  cardName.innerHTML = value;
+};
+
+inputName.addEventListener("change", setReceiver);
